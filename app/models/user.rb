9f0618b981d7db02789c723ee32337fc49ff94ca
karlos <karloszuru@gmail.com>
2024-09-05 17:13:04 -0500

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 #  has_many :posts
 #  has_many :comments
 #add another comment. Yeah
-
+validates :first_name, presence: true
+validates :last_name, presence: true
 
 end
